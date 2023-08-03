f87bf36c610c9756f677c0817438fbad2b559241
robinoldman <robin.c.oldman@gmail.com>
2023-08-03 09:50:32 +0000

    add nav bar css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assests/logo-medium.png";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+            <i class="fa-solid fa-right-to-bracket"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
