24e5f438e4e4f5576d23705caefa81d3c5f4c975
robinoldman <robin.c.oldman@gmail.com>
2023-08-03 07:56:22 +0000

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Button from "react-bootstrap/Button";
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-<h1>hello world</h1>
+      <NavBar />
     </div>
   );
 }
