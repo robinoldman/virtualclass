ba59f6a99038d496dadaa38c04bf568abfa900c1
robinoldman <robin.c.oldman@gmail.com>
2023-08-11 13:06:10 +0000

    in currentusercontext gave access and refresh tokens for a user to stay logged in

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,4 +1,8 @@
-import axios from 'axios';
-axios.defaults.baseURL = 'https://virtualclassapi-2c20ab038085.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
