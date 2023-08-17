6b665f7561435d17ce93a59bae7c193838488f1a
robinoldman <robin.c.oldman@gmail.com>
2023-08-17 16:12:30 +0000

    added click handler to check when the user clicks away from the burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,13 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from './Avatar'; 
 import axios from "axios";
+import useClicksOUtsideToggle from "../hooks/useClicksOUtsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useClicksOUtsideToggle();
 
 
   const handleSignOut = async () => {
@@ -85,7 +88,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={ expanded } className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -93,7 +96,10 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle 
+        ref={ref}
+        onClick={() => setExpanded(!expanded)} 
+        aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
