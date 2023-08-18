5850f212a7951e7b4bcb36c48d3f33f043003fea
robinoldman <robin.c.oldman@gmail.com>
2023-08-18 15:39:40 +0000

    add ability to add images name and title to post

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -11,6 +11,8 @@ import Upload from "../../assets/upload.png";
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Assets";
+import { Image } from "react-bootstrap";
 
 
 
@@ -18,10 +20,59 @@ function PostCreateForm() {
 
   const [errors, setErrors] = useState({});
 
+  const [postData, setPostData] = useState({
+    title: "",
+    image: "",
+    content: "",
+  });
+  const { title, content, image } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+
+
 
   const textFields = (
-    <div className="text-center">
-      {/* Add your form fields here */}
+      <div className="text-center">
+          <Form.Group className="text-center">
+              
+              <Form.Label>
+                Title
+              </Form.Label>
+              <Form.Control 
+              type="text" 
+              name="title"
+              value={title}
+              onChange={handleChange} />
+          </Form.Group>
+
+          <Form.Group className="text-center">
+              
+              <Form.Label>
+                Content
+              </Form.Label>
+              <Form.Control 
+              rows={6}  
+              as='textarea' 
+              type="text"
+              value={content}
+              onChange={handleChange}/>
+          </Form.Group>
 
     
     
@@ -37,6 +88,7 @@ function PostCreateForm() {
     </div>
   );
 
+
   return (
     <Form>
       <Row>
@@ -45,14 +97,37 @@ function PostCreateForm() {
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
                 <Form.Label
                   className="d-flex justify-content-center"
                   htmlFor="image-upload"
                 >
-                  ASSET
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to upload an image"
+                  />
                 </Form.Label>
+              )}
 
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+              />
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
           </Container>
