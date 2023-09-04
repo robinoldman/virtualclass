479196acf42ac65a9a75654228a6a298d73b3b0e
robinoldman <robin.c.oldman@gmail.com>
2023-09-04 14:38:52 +0000

    delete unwanted front end extra file

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ['DEBUG'] = '1'  
+os.environ['CLOUDINARY_URL'] = "cloudinary://962444711949455:IEF70dVolBgsnC0nr_HzBPQZYuA@dwpdkpzlx"
+os.environ['SECRET_KEY'] = "SecretKeyRobinOldman"
+os.environ['ALLOWED_HOST'] = 
+#os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = 'postgres://wxnltetf:KWYZI9A94_0gTnTjmjeA7Qb5yGyJWd6_@surus.db.elephantsql.com/wxnltetf'
